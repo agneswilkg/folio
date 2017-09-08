e2eabed4e2ad64a061c31eb45dc81352a02f0d7d
agneswilkg <wilkmademe@gmail.com>
2017-09-08 12:31:17 +0200

    add watchesrs for .js and .html files

--- gulpfile.js
+++ gulpfile.js
@@ -27,4 +27,6 @@ gulp.task('browserSync', function(){
 gulp.task('watch', ['browserSync', 'sass'], function(){
   gulp.watch('app/scss/**/*.scss', ['sass']);
   //other watchers
+  gulp.watch('app/*.html', browserSync.reload);
+  gulp.watch('app/js/**/*.js', browserSync.reload);
 });
